4bc9199638694d2c413552134f622c69f59fc645
DEXV <89728480+DXVVAY@users.noreply.github.com>
2024-11-09 02:44:32 +0100

    Update api_use.py

--- api_use.py
+++ api_use.py
@@ -5,5 +5,5 @@ data = {
     "version": "0d69d1a359119bd0e2c5ca7f11f300ac050517fd19b612f86c0c75a2b0b39cbe"
 }
 
-result = requests.post("http://127.0.0.1:4201/encrypt_blob", json=data).json()
+result = requests.post("http://127.0.0.1:4200/encrypt_blob", json=data).json()
 print(result.get("result", "Failed"))
