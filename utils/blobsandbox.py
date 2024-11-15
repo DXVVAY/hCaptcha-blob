67fe3f4f84fd68cce773780233187b426c72eb99
DEXV <89728480+DXVVAY@users.noreply.github.com>
2024-11-15 20:19:50 +0100

    Update blobsandbox.py

--- utils/blobsandbox.py
+++ utils/blobsandbox.py
@@ -30,13 +30,13 @@ class BlobSandbox:
             if 'function' in line and ', [2, ' in line and 'Promise' not in line:
                 name = line.split(', [2, ')[1].split('(')[0]
                 log.info(f"Encryption function name -> {name}")
-            if 'return ' in line and '(JSON.stringify(' in line:
+            if 'if (0 === A) return' in line:
                 parts = line.split('return ')
                 if len(parts) == 2 and name:
-                    lines[i] = f'            return {name}(new Uint8Array(Q.split("").map(char => char.charCodeAt(0))))'
+                    lines[i] = f'            if (0 === A) return {name}(new Uint8Array(Q.split("").map(char => char.charCodeAt(0))))'
 
         self.hsw = '\n'.join(lines)
-        self.hsw += '\nasync function encrypt(data) {\n\n    return await hsw("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmIjowLCJzIjoyLCJ0IjoidyIsImQiOiIyZEhTZEpSYjdwelhPSTNJRnB2aEdmdmJqbm1ybXhqYkhCZ2JPU01BYTNuOGVaR2RUWGV4TTZVVE1KUllKRlYrdXoyc1hnZVNyQmxqSEM3T0RGUXVIVW1VekpkMk4zMlVPdUw4UDJNaU0zZFZmbGZJVUZLS1hKYVdZUTloanM4R1BQYXdnTm54UUtaTS92RGUrQ2g2QUxnTzdxNWV2QjREc214TVI4YXIrWThBTTZmSXFCZzV2L1BreUhsYzlKQnd2MnZQUWlNOFgxczdWc2MrMXZOdGZYMFFzUDdXZGdFK2svNXlQQTVIK2tGUmROQ011WW4weVBidEhIRG80MW5CRzdMOCIsImwiOiJodHRwczovL25ld2Fzc2V0cy5oY2FwdGNoYS5jb20vYy8wZGNiZTM3IiwiaSI6InNoYTI1Ni1TRUI0SjZST09JOEl5bXRDU2tNd3REdk8yUCtQVUNvaXRKZ2d5MXVrU21RPSIsImUiOjE3MTk4NDAzNDQsIm4iOiJoc3ciLCJjIjoxMDAwfQ.MkmmdL1vyLRtkKYwKtYkzVNYq1dNzdexuxoCN28I1Mg", data);\n}'
+        self.hsw += '\nasync function encrypt(data) {\n\n    return await hsw(0, data);\n}'
         with open("custom_hsw.js", "w", encoding='utf-8') as f: f.write(self.hsw)
         self.archive("custom_hsw.js")
         self.archive("original_hsw.js")
